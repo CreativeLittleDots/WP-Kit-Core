4a66af09fd8d49bcf422ab4e6b428b41ea9bb8c9
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-01-19 19:57:47 +0000

    Required plugins fix

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -769,9 +769,6 @@
 					tgmpa( $plugins, 
 						array(
 					        'domain'		=> 'wpkit', // Text domain - likely want to be the same as your theme.
-					        'default_path'		=> '', // Default absolute path to pre-packaged plugins
-					        'parent_menu_slug'	=> 'themes.php', // Default parent menu slug
-					        'parent_url_slug'	=> 'themes.php', // Default parent URL slug
 					        'menu'			=> 'install-required-plugins', // Menu slug
 					        'has_notices'		=> true, // Show admin notices or not
 					        'is_automatic'		=> false, // Automatically activate plugins after installation or not

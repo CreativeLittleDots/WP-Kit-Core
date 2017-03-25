63560847e2084ec5943fd0144b046a1c92027c22
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-03-25 16:05:56 +0000

    Router dispatch on action

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -2,10 +2,19 @@
     
     namespace WPKit\Core;
     
+    use Illuminate\Http\Request;
     use Illuminate\Routing\Router as BaseRouter;
     
     class Router extends BaseRouter {
 		
+		 /**
+	     * Register a new route with the given verbs.
+	     *
+	     * @param  string  $path
+	     * @param  \Closure|array|string|null  $callback
+	     * @param  array|string  $methods
+	     * @return \Illuminate\Routing\Route
+	     */
 		public function map( $path, $callback, $method = 'get' ) {
 			
 			$methods = $method == '*' ? [
@@ -18,18 +27,26 @@
 			
 			$methods = array_map( 'strtoupper', is_array( $methods ) ? $methods : array( $methods ) );
 			
-			$route = $this->createRoute( $methods, $path, $callback );
-			
-			add_action( 'init', function() use ( $route ) {
-				
-				$this->routes->add( $route );
-				
-				$this->dispatch();
-				
-			} );
-			
-			return $route;
+			return $this->addRoute( $methods, $path, $callback );
 			
 		}
+		
+		/**
+	     * Dispatch the request to the application.
+	     *
+	     * @param  \Illuminate\Http\Request  $request
+	     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+	     */
+	    public function dispatch(Request $request) {
+		    
+		    add_action( 'init', function() {
+			    
+			    $this->currentRequest = $request;
+			    
+				return $this->dispatchToRoute($request);
+			    
+		    });
+	        
+	    }
 	    
 	}

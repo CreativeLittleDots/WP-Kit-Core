44780385b5245d0eda51261010c45eac69a48f25
Terence O'Donoghue <terence@creativelittledots.co.uk>
2017-09-09 11:36:41 +0100

    Update Application.php

--- src/app/Foundation/Application.php
+++ src/app/Foundation/Application.php
@@ -28,7 +28,7 @@ class Application extends Container implements ApplicationContract, HttpKernelIn
      *
      * @var string
      */
-    const VERSION = '5.4.17';
+    const VERSION = '1.5.8';
 
     /**
      * The base path for the Laravel installation.
